488375d24555baa64771e8753489ca478b8e63af
Max Lemieux <max.lemieux@gmail.com>
2020-04-11 20:26:17 -0700

    updated SQL seed

--- db/seed.sql
+++ db/seed.sql
@@ -10,7 +10,11 @@ INSERT INTO role (title, department_id, salary) VALUES ('CEO', 1, 250000);
 INSERT INTO role (title, department_id, salary) VALUES ('VP Sales', 2, 150000);
 INSERT INTO role (title, department_id, salary) VALUES ('VP Operations', 3, 150000);
 INSERT INTO role (title, department_id, salary) VALUES ('Chief Bottle Washer', 3, 50000);
+INSERT INTO role (title, department_id, salary) VALUES ('Novice Bottle Washer', 3, 25000);
 
 # Employees
-INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Fred', 'Flintstone', null, 1);
-INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Barney', 'Rubble', null, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Wilma', 'Flintstone', null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Fred', 'Flintstone', 1, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Betty', 'Barney', 1, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Barney', 'Rubble', 3, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Dino', 'Flintstone', 3, 5);
